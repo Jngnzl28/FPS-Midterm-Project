33bea7e038f5db005dbc0dd1a465a91eff75d093
Jngnzl28 <jngnzles@gmail.com>
2024-03-14 10:34:36 -0700

    Fireball Setup

---
+++ Assets/Scripts/FireShooter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireShooter : MonoBehaviour
+{
+    private Camera cam;
+
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+
+}
